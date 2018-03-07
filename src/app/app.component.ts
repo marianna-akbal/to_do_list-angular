6a3e2a137660470760ed12a6fcf315a19e046a71
Marianna Konarska - Czerwiakowska <marianna@MacBook-Pro-Marianna.local>
2018-03-07 10:38:28 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-root',
+  template: `
+    <h1>
+      {{title}}
+    </h1>
+
+    <todo-input></todo-input>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+}
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-input',
+  template: `
+    <input>
+    <button>Save</button>
+    <p>The title is: {{ title }}</p>
+  `,
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  title: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
